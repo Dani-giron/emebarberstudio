455705c5d4adb5cc036010eacb8417f599a78275
dani-giron <danigrausds@gmail.com>
2026-05-15 15:05:51 +0200

    ayuda

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,6 +32,11 @@ export default function Footer() {
               <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M19.59 6.69a4.83 4.83 0 01-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 01-2.88 2.5 2.89 2.89 0 01-2.89-2.89 2.89 2.89 0 012.89-2.89c.28 0 .54.04.79.1V9.01a6.29 6.29 0 00-.79-.05 6.34 6.34 0 00-6.34 6.34 6.34 6.34 0 006.34 6.34 6.34 6.34 0 006.33-6.34V8.67a8.21 8.21 0 004.8 1.54V6.69a4.85 4.85 0 01-1.03 0z"/></svg>
             </a>
           </div>
+          <img
+            src="/images/ayuda.jpeg"
+            alt="Ayuda reserva"
+            className={styles.ayudaImg}
+          />
         </div>
 
         {/* Navegación */}
